f1b76d9480ed8b313ea1bab82e04172cc090b59a
Saturn-98 <ishaan.3098@gmail.com>
2025-08-20 13:41:53 +0530

    separating modules

--- src/app/traffic/traffic.component.ts
+++ src/app/traffic/traffic.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { type TrafficData } from './traffic.model';
 
 @Component({
   selector: 'app-traffic',
@@ -9,7 +8,36 @@ import { type TrafficData } from './traffic.model';
   styleUrl: './traffic.component.css'
 })
 export class TrafficComponent {
-  @Input() dummyTrafficData?: TrafficData[];
-  @Input() maxTraffic!: number;
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
 
 }

d3044ec38b7414d29241af637faed9e4fdaabf74
Saturn-98 <ishaan.3098@gmail.com>
2025-09-04 13:50:05 +0530

    adding styles

---
+++ src/app/shared/controls/controls.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-controls',
+  standalone: true,
+  imports: [],
+  templateUrl: './controls.component.html',
+  styleUrl: './controls.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    'class': 'control'
+  }
+})
+export class ControlsComponent {
+  @Input({required: true}) controlTitle!: string;
+
+}
